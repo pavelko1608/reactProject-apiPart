95fb4ef820a8f7b109a71d791fdecc98ddabcb4c
Pavlo Paliychuk <pavelko1608@gmail.com>
2017-06-05 21:43:30 +0300

    port fix

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var bodyParser = require("body-parser");
 var mongo = require("mongodb");
 var mongoose = require("mongoose");
 
-mongoose.connect("mongodb://127.0.0.1:27017/react-router");
+mongoose.connect("mongodb://localhost/react-router");
 var db = mongoose.connection;
 
 var routes = require("./routes/routes");
--- models/car.js
+++ models/car.js
@@ -1,7 +1,7 @@
 var mongoose = require("mongoose");
 var autoIncrement = require('mongoose-auto-increment');
 var Schema = mongoose.Schema;
-var connection = mongoose.createConnection("mongodb://127.0.0.1:27017/react-router");
+var connection = mongoose.createConnection("mongodb://localhost/react-router");
 autoIncrement.initialize(connection);
 
 
